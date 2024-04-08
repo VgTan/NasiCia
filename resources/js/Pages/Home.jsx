b415b7fa656ee52f837a782e5534d933b6842a46
VgTan <alfonsusvega9@gmail.com>
2024-04-08 12:24:24 +0700

    feat: test route

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="flex justify-center">
+            <div className="">
+                <span>Halo</span>
+            </div>
+        </div>
+    )
+}
